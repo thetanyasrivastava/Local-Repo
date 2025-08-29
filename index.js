027ff84ed6288d07eb0bca11cbf645c212ba768d
Tanya Srivastava <thetanya02@gmail.com>
2025-08-29 19:08:15 +0530

    Using Postman

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("Hello World!");
-console.log("Hello Local Repo!");
+// console.log("Hello World!");
+// console.log("Hello Local Repo!");
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) => {
+    res.send("Home Page")
+})
+
+app.post("/register", (req,res) => {
+    res.sendStatus(201);
+})
+
+app.put("/user", (req,res) => {
+    res.sendStatus(200);
+})
+
+app.patch("/user/about", (req,res) => {
+    res.sendStatus(200);
+})
+
+app.listen(port, () => {
+    console.log('Server running on port ', port);
+})
